b05381d986e734883e352029e76a4ce1215c1a49
Arno Dirlam <arnodirlam@googlemail.com>
2021-02-01 19:06:19 +0100

    Add `{:ref, path}` value lookups in conditions

--- lib/infer/engine.ex
+++ lib/infer/engine.ex
@@ -3,9 +3,15 @@ defmodule Infer.Engine do
   Encapsulates the main functionality of working with rules.
   """
 
+  alias Infer.Util
+
   def rules_for_predicate(predicate, type) do
-    type.infer_rules()
-    |> Enum.filter(&(&1.key == predicate))
+    if Util.Module.has_function?(type, :infer_rules, 0) do
+      type.infer_rules()
+      |> Enum.filter(&(&1.key == predicate))
+    else
+      []
+    end
   end
 
   def resolve_predicate(predicate, subject = %type{}) do
@@ -17,35 +23,45 @@ defmodule Infer.Engine do
   def match_rules([], _record), do: nil
 
   def match_rules([rule | rules], record) do
-    if evaluate_condition(rule.when, record) do
+    if evaluate_condition(rule.when, record, record) do
       rule.val
     else
       match_rules(rules, record)
     end
   end
 
-  def evaluate_condition(conditions, subject) when is_list(conditions) do
-    Enum.any?(conditions, &evaluate_condition(&1, subject))
+  def evaluate_condition(condition, subjects, root_subject) when is_list(subjects) do
+    Enum.any?(subjects, &evaluate_condition(condition, &1, root_subject))
   end
 
-  def evaluate_condition(conditions, subject) when is_map(conditions) do
-    Enum.all?(conditions, &evaluate_condition(&1, subject))
+  def evaluate_condition(conditions, subject, root_subject) when is_list(conditions) do
+    Enum.any?(conditions, &evaluate_condition(&1, subject, root_subject))
   end
 
-  def evaluate_condition({key, sub_condition}, subject = %type{}) do
+  def evaluate_condition(conditions, subject, root_subject) when is_map(conditions) do
+    Enum.all?(conditions, &evaluate_condition(&1, subject, root_subject))
+  end
+
+  def evaluate_condition({key, sub_condition}, subject = %type{}, root_subject) do
     key
     |> rules_for_predicate(type)
     |> case do
-      [] -> evaluate_condition(sub_condition, Map.get(subject, key))
+      [] -> evaluate_condition(sub_condition, Map.get(subject, key), root_subject)
       rules -> match_rules(rules, subject)
     end
   end
 
-  def evaluate_condition({:not, condition}, subject) do
-    not evaluate_condition(condition, subject)
+  def evaluate_condition({:not, condition}, subject, root_subject) do
+    not evaluate_condition(condition, subject, root_subject)
   end
 
-  def evaluate_condition(predicate, subject = %type{}) when is_atom(predicate) do
+  def evaluate_condition({:ref, path}, subject, root_subject) do
+    root_subject
+    |> get_in_path(path)
+    |> evaluate_condition(subject, root_subject)
+  end
+
+  def evaluate_condition(predicate, subject = %type{}, _root_subject) when is_atom(predicate) do
     predicate
     |> rules_for_predicate(type)
     |> case do
@@ -54,7 +70,10 @@ defmodule Infer.Engine do
     end
   end
 
-  def evaluate_condition(other, subject) do
+  def evaluate_condition(other, subject, _root_subject) do
     subject == other
   end
+
+  defp get_in_path(val, []), do: val
+  defp get_in_path(map, [key | path]), do: Map.get(map, key) |> get_in_path(path)
 end
---
+++ lib/infer/util/module.ex
@@ -0,0 +1,7 @@
+defmodule Infer.Util.Module do
+  @moduledoc """
+  Utility functions for working with modules and their functions.
+  """
+
+  defdelegate has_function?(module, function_name, arity), to: Kernel, as: :function_exported?
+end
