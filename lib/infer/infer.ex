0f13fed411f2b4a9863b96046d7eee6f7dad18eb
Arno Dirlam <arnodirlam@googlemail.com>
2021-05-28 14:51:58 +0200

    Revert "Introduce `Eval.load_while_data_reqs/2`"
    This reverts commit ea892df273b4577a8427a2375fbe66d8e4cb35d0.

--- lib/infer/infer.ex
+++ lib/infer/infer.ex
@@ -295,9 +295,16 @@ defmodule Infer do
   Like `get/3`, but loads additional data if needed.
   """
   def load(records, predicates, opts \\ []) do
-    Eval.load_while_data_reqs(opts, fn eval ->
-      do_get(records, predicates, eval)
-    end)
+    eval = Eval.from_options(opts)
+
+    do_load(eval, &do_get(records, predicates, &1))
+  end
+
+  defp do_load(eval, fun) do
+    case fun.(eval) do
+      {:not_loaded, data_reqs} -> Eval.load_data_reqs(eval, data_reqs) |> do_load(fun)
+      result -> result
+    end
   end
 
   @doc """
