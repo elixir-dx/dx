d42fa210b491b7d9569f3e9437c48d9aab2bd12b
Arno Dirlam <arnodirlam@googlemail.com>
2021-12-07 18:34:28 +0100

    Support complex conditions in query operators

--- lib/infer/ecto/query.ex
+++ lib/infer/ecto/query.ex
@@ -311,10 +311,22 @@ defmodule Infer.Ecto.Query do
   """
   def apply_options(queryable, opts) do
     Enum.reduce(opts, {queryable, []}, fn
-      {:where, conditions}, {query, opts} -> {filter_by(query, conditions), opts}
-      {:limit, limit}, {query, opts} -> {limit(query, limit), opts}
-      {:order_by, order}, {query, opts} -> {order_by(query, order), opts}
-      other, {query, opts} -> {query, [other | opts]}
+      {:where, conditions}, {query, opts} when is_list(conditions) ->
+        {where(query, {:all, conditions}), opts}
+
+      {:where, conditions}, {_query, _opts} ->
+        raise ArgumentError,
+              "Expected a list of conditions in Infer query operator. Got " <>
+                inspect(conditions, pretty: true)
+
+      {:limit, limit}, {query, opts} ->
+        {limit(query, limit), opts}
+
+      {:order_by, order}, {query, opts} ->
+        {order_by(query, order), opts}
+
+      other, {query, opts} ->
+        {query, [other | opts]}
     end)
     |> case do
       {queryable, opts} -> {queryable, Enum.reverse(opts)}
@@ -325,19 +337,6 @@ defmodule Infer.Ecto.Query do
   def from_options(queryable, opts) do
     {queryable, []} = apply_options(queryable, opts)
     queryable
-  end
-
-  def filter_by(queryable, conditions) do
-    Enum.reduce(conditions, queryable, fn
-      {key, nil}, queryable ->
-        from(q in queryable, where: is_nil(field(q, ^key)))
-
-      {key, list}, queryable when is_list(list) ->
-        from(q in queryable, where: field(q, ^key) in ^list)
-
-      {key, val}, queryable ->
-        from(q in queryable, where: field(q, ^key) == ^val)
-    end)
   end
 
   def limit(queryable, limit) do
