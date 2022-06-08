ae316f186dcdfd0fd1596c2552b98757d1e5b1ce
Arno Dirlam <arnodirlam@googlemail.com>
2022-06-08 19:50:48 +0200

    Deps: Use extracted Refinery library for generating test data recursively

--- mix.exs
+++ mix.exs
@@ -60,6 +60,7 @@ defmodule Infer.MixProject do
       # dev & test
       {:postgrex, "~> 0.14", only: :test, runtime: false},
       {:timex, "~> 3.6", only: :test, runtime: false},
+      {:refinery, "~> 0.1.0", github: "infer-beam/refinery", only: :test},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
