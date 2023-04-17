4787aa3123fcdac73c2358e9f3c8336626a57c32
Fredrik Teschke <f@ftes.de>
2023-04-17 12:00:33 +0200

    Pretty print error.
    Co-authored-by: Arno Dirlam <arnodirlam@googlemail.com>

--- lib/dx/error/error.ex
+++ lib/dx/error/error.ex
@@ -2,6 +2,6 @@ defmodule Dx.Error do
   defexception [:cause]
 
   def message(error) do
-    "Error occurred: #{inspect(error.cause)}"
+    "Error occurred: #{inspect(error.cause, pretty: true)}"
   end
 end
