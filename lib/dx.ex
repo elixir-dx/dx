d2c24e46cec68dd46de6751876bbad58af3caeb3
Fredrik Teschke <f@ftes.de>
2023-04-19 14:38:20 +0200

    Document loader_options in top-level Dx module

--- lib/dx.ex
+++ lib/dx.ex
@@ -34,7 +34,10 @@ defmodule Dx do
   - **return_cache** (boolean) makes non-bang functions return `{:ok, result, cache}` instead of
     `{:ok, result}` on success. This `cache` can be passed to other Dx functions (see `cache` option)
   - **cache** (`Dataloader` struct) can be used to pass in an existing cache, so data already loaded
-    doesn't need to be loaded again. Can be initialized using `Loaders.Dataloader.init/0`.
+    doesn't need to be loaded again. Can be initialized using `Dx.Loaders.Dataloader.init/0`.
+  - **loader** allows choosing a loader module. Defaults to `Dx.Loaders.Dataloader`.
+  - **loader_options** are passed to `loader.init/1` function. See `Dx.Loaders.Dataloader` for options
+    supported by the default loader.
   """
 
   alias Dx.{Engine, Result, Util}
