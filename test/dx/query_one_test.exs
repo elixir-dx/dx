460c62bd0bf268efebf5db61702748972128159a
Fredrik Teschke <f@ftes.de>
2023-04-20 07:31:56 +0200

    Format

--- test/dx/query_one_test.exs
+++ test/dx/query_one_test.exs
@@ -73,7 +73,9 @@ defmodule Dx.QueryOneTest do
       refute_received {:ecto_query, %{source: "list_calendar_overrides"}}
     end
 
-    test "raises Dx.Error.Timeout if configurable dataloader timeout is exceeded", %{tasks: [task | _]} do
+    test "raises Dx.Error.Timeout if configurable dataloader timeout is exceeded", %{
+      tasks: [task | _]
+    } do
       loader_opts = [timeout: 0]
 
       assert_raise Dx.Error.Timeout, fn ->
